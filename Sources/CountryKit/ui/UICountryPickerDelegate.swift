776e478a0182c29b52161614b96052975f3e8696
Deniz <deniz@eclypse.io>
2024-01-08 15:30:10 -0600

    made protocol methods optional (#17)

--- Sources/CountryKit/ui/UICountryPickerDelegate.swift
+++ Sources/CountryKit/ui/UICountryPickerDelegate.swift
@@ -8,23 +8,32 @@
 import Foundation
 
 
-/// Conform to this protocol if you want to be notified every time user makes a selection or
-/// a deselection in the UI. The events in this protocol trigger right away after a selection
-/// is made.
+/// This protocol triggers events when user makes a selection or a deselection in the PickerView UI.
+/// All methods in this protocol are optional.
+/// If you need immediate callback as soon as user makes a selection, then override didSelect(country:) and didDeselect(country:) methods.
+/// If you want to wait for the user to finish making their selections and get notified of all the selections when the pickerview is dismissed, then
+/// override didFinishSelecting(countries:) method.
 public protocol UICountryPickerDelegate: AnyObject {
-    /// This event fires when user selects a country
+    /// This event fires when user selects a country. Optional.
     /// - Parameter country: The country object that was selected
     func didSelect(country: Country)
     
-    /// This event fires when user reverses a previous country selection
+    /// This event fires when user reverses a previous country selection. Optional.
     /// - Parameter country: The country object that was deselected
     func didDeselect(country: Country)
     
-    /// List of all countries user has selected at the end of their interaction with UICountryPicker.
-    /// This event is fired when the view controller is being dismissed
+    /// List of all countries user has selected at the end of their interaction with UICountryPicker. Optional.
+    /// This event is fired when the view controller is being dismissed.
     /// - Parameter countries: List of all countries user has selected
     func didFinishSelecting(countries: [Country])
     
-    /// User clicked back to return to previous screen
+    /// User clicked back/cancel to return to previous screen. Optional.
     func didCancel()
+}
+
+public extension UICountryPickerDelegate {
+    func didSelect(country: Country) {}
+    func didDeselect(country: Country) {}
+    func didFinishSelecting(countries: [Country]) {}
+    func didCancel() {}
 }
